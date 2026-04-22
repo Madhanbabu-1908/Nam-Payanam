fbd73be9f49f492b4c02a9e04bb18d2c03afeb21
Madhanbabu-1908 <89144800+Madhanbabu-1908@users.noreply.github.com>
2026-04-22 05:49:56 +0530

    Update ItineraryPage.tsx

--- frontend/src/pages/ItineraryPage.tsx
+++ frontend/src/pages/ItineraryPage.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { api } from '../config/api';
 import { ArrowLeft, MapPin, Clock, Sun, Moon, Coffee, Download } from 'lucide-react';
